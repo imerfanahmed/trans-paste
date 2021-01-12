9864c94d4e373fbe57ffafbc2d48c93c5aaf189a
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2021-01-12 21:38:46 +0600

    Client Updated

--- client.py
+++ client.py
@@ -1,10 +1,12 @@
 import socket
+import time
+import pyperclip as pp
 c=socket.socket()
-
 c.connect(('192.168.1.89',9989))
-
-
-while(True):
-    text=input("Type your Massage here= ")
-    c.send(bytes(text,'utf-8'))
-    print(c.recv(1024).decode())
+while True:
+    clip=pp.paste()
+    newclip=clip
+    if(clip!=newclip):
+        c.send(bytes(newclip,'utf-8'))
+    newclip=pp.paste()
+    
