82a97ac417b56faefb0d86cd09b50e43de06ac11
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2021-01-13 13:46:59 +0600

    Server File Updated

--- client.py
+++ client.py
@@ -1,12 +1,15 @@
 import socket
-import time
-import pyperclip as pp
+import pyperclip as ppc
 c=socket.socket()
 c.connect(('192.168.1.89',9989))
-while True:
-    clip=pp.paste()
-    newclip=clip
-    if(clip!=newclip):
-        c.send(bytes(newclip,'utf-8'))
-    newclip=pp.paste()
+
+def data():
+   text=ppc.waitForNewPaste()
+   return text
     
+
+
+while True:
+    c.send(bytes(data(),'utf-8'))
+    
+        
